1937b804074ec80a040ef4c4615a108b4deadc37
Slava Golyshevskii <viacheslav.golyshevskiy@gamil.com>
2022-11-28 11:18:00 +0400

    wiki

---
+++ dags/wikipedia_pageviews/sql/insert_pageviews.sql
@@ -0,0 +1,6 @@
+insert into pageviews_count (pagename, pageviews, date) 
+            values ('Apple', '57', '2022-11-28T06:51:15.356491+00:00');
+insert into pageviews_count (pagename, pageviews, date) 
+            values ('Google', '818', '2022-11-28T06:51:15.356491+00:00');
+insert into pageviews_count (pagename, pageviews, date) 
+            values ('Amazon', '8', '2022-11-28T06:51:15.356491+00:00');
